772e65a3e159b7e2f9cce8e4e73913c751323c50
Suraj Pal <surajpal7442@gmail.com>
2025-10-09 19:31:44 +0530

    update js

--- app.js
+++ app.js
@@ -20,10 +20,24 @@ app.get('/login', (req, res) => {
     res.render("login");
 });
 
-app.get('/profile', isLoggedIn, (req,res) => {
-    console.log(req.user);
-    res.render("login");
-})
+app.get('/profile', isLoggedIn, async (req,res) => {
+    let user = await userModel.findOne({email: req.user.email}).populate("posts");
+    res.render("profile", {user});
+});
+
+app.get('/post', isLoggedIn, async (req, res) => {
+    let user = await userModel.findOne({email: req.user.email});
+    let {content} = req.body;
+
+    let post = await postModel.create({
+        user: user._id,
+        content
+    });
+
+    user.posts.push(post._id);
+    await user.save();
+    res.redirect('/profile');
+});
 
 app.post('/register', async (req, res)=> {
     let {username, name, age, email, password} = req.body;
@@ -57,7 +71,7 @@ app.post('/login', async (req, res) => {
         if(result) {
             let token = jwt.sign({email: email, userid: user._id}, JWT_SECRET);
             res.cookie("token", token);
-            res.status(200).send("You can login");
+            res.status(200).redirect("/profile");
         }
         else res.redirect("/login");
     })
@@ -69,7 +83,7 @@ app.get('/logout', (req, res) => {
 });
 
 function isLoggedIn(req, res, next){
-    if(req.cookies.token === "") res.send("You must be logged in");
+    if(req.cookies.token === "") res.redirect("/login");
     else{
         let data = jwt.verify(req.cookies.token, JWT_SECRET);
         req.user = data;
